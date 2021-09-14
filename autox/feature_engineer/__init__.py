77bc1761c50bda21fce5bc21fff4115dd3d11443
poteman <946691288@qq.com>
2021-09-14 16:08:56 +0800

    init FeatureDimensionReduction.

--- autox/feature_engineer/__init__.py
+++ autox/feature_engineer/__init__.py
@@ -2,3 +2,4 @@ from .fe_count import FeatureCount
 from .fe_stat import FeatureStat
 from .fe_rank import FeatureRank
 from .fe_target_encoding import FeatureTargetEncoding
+from .fe_dimension_reduction import FeatureDimensionReduction
