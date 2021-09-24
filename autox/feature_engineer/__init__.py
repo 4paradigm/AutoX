db903d295e59e38c749ac45381347561caf40545
poteman <946691288@qq.com>
2021-09-24 17:18:50 +0800

    init FeatureDenoisingAutoencoder.

--- autox/feature_engineer/__init__.py
+++ autox/feature_engineer/__init__.py
@@ -4,3 +4,4 @@ from .fe_rank import FeatureRank
 from .fe_target_encoding import FeatureTargetEncoding
 from .fe_dimension_reduction import FeatureDimensionReduction
 from .fe_nlp import FeatureNlp
+from .fe_denoising_autoencoder import FeatureDenoisingAutoencoder
