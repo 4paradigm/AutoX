c4ee0ccef973b048021e221c47eebbd19ff2e036
poteman <946691288@qq.com>
2021-10-29 13:03:12 +0800

    编辑当前数据集.

--- run.py
+++ run.py
@@ -16,13 +16,75 @@ print("path_output: ", path_output)
 # 配置数据信息, 选择数据集
 data_name = path_input.split('/')[-1]
 
-# allstate_claims
-# autox = AutoX(target = 'loss', train_name = 'train.csv', test_name = 'test.csv',
-#                id = ['id'], path = path_input, metric = 'mae')
-
-# stumbleupon
-autox = AutoX(target = 'label', train_name = 'train.csv', test_name = 'test.csv',
-               id = ['urlid'], path = path_input)
+if data_name == 'kaggle_tabular_aug_2021':
+    autox = AutoX(target='loss', train_name='train.csv', test_name='test.csv',
+                    id=['id'], path = path_input)
+elif data_name == 'kaggle_house_price':
+    autox = AutoX(target='SalePrice', train_name='train.csv', test_name='test.csv',
+                    id=['Id'], path = path_input)
+elif data_name == 'titanic':
+    autox = AutoX(target='Survived', train_name='train.csv', test_name='test.csv',
+                  id=['PassengerId'], path = path_input)
+elif data_name == 'kaggle_ieee':
+    relations = [
+        {
+            "related_to_main_table": "true",  # 是否为和主表的关系
+            "left_entity": "train_transaction.csv",  # 左表名字
+            "left_on": ["TransactionID"],  # 左表拼表键
+            "right_entity": "train_identity.csv",  # 右表名字
+            "right_on": ["TransactionID"],  # 右表拼表键
+            "type": "1-1"  # 左表与右表的连接关系
+        },
+        {
+            "related_to_main_table": "true",  # 是否为和主表的关系
+            "left_entity": "test_transaction.csv",  # 左表名字
+            "left_on": ["TransactionID"],  # 左表拼表键
+            "right_entity": "test_identity.csv",  # 右表名字
+            "right_on": ["TransactionID"],  # 右表拼表键
+            "type": "1-1"  # 左表与右表的连接关系
+        }
+    ]
+    autox = AutoX(target='isFraud', train_name='train_transaction.csv', test_name='test_transaction.csv',
+                  id=['TransactionID'], path = path_input, relations=relations)
+elif data_name == 'kaggle_springleaf':
+    autox = AutoX(target='target', train_name='train.csv', test_name='test.csv',
+                  id=['ID'], path = path_input)
+elif data_name == 'stumbleupon':
+    autox = AutoX(target = 'label', train_name = 'train.csv', test_name = 'test.csv',
+                    id = ['urlid'], path = path_input)
+elif data_name == 'santander':
+    autox = AutoX(target='target', train_name='train.csv', test_name='test.csv',
+                  id=['ID_code'], path = path_input)
+elif data_name == 'ventilator':
+    feature_type = {
+        'train.csv': {
+            'id': 'cat',
+            'breath_id': 'cat',
+            'R': 'num',
+            'C': 'num',
+            'time_step': 'num',
+            'u_in': 'num',
+            'u_out': 'num',
+            'pressure': 'num'
+        },
+        'test.csv': {
+            'id': 'cat',
+            'breath_id': 'cat',
+            'R': 'num',
+            'C': 'num',
+            'time_step': 'num',
+            'u_in': 'num',
+            'u_out': 'num'
+        }
+    }
+    autox = AutoX(target='pressure', train_name='train.csv', test_name='test.csv',
+                  id=['id'], path = path_input, feature_type=feature_type, metric='mae')
+elif data_name == 'allstate_claims':
+    autox = AutoX(target = 'loss', train_name = 'train.csv', test_name = 'test.csv',
+                    id = ['id'], path = path_input, metric = 'mae')
+elif data_name == 'RestaurantRevenue':
+    autox = AutoX(target='revenue', train_name='train.csv', test_name='test.csv',
+                  id=['Id'], path = path_input)
 
 sub = autox.get_submit()
 
