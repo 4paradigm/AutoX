70bccfc846e8caff59e47337bf7fd17dbab175a0
poteman <946691288@qq.com>
2021-11-08 16:28:09 +0800

    init 时序模型.

--- autox/models/__init__.py
+++ autox/models/__init__.py
@@ -1,2 +1,3 @@
 from .regressor import CrossLgbRegression, CrossXgbRegression
+from .regressor_ts import LgbRegressionTs, XgbRegressionTs
 from .classifier import CrossLgbBiClassifier, CrossXgbBiClassifier
