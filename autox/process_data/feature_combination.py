7c86e33f43a0547a927b79c3f88ef18b63c4128b
poteman <946691288@qq.com>
2021-11-09 15:12:00 +0800

    优化feature_combination显示.

--- autox/process_data/feature_combination.py
+++ autox/process_data/feature_combination.py
@@ -1,8 +1,9 @@
 import pandas as pd
+from tqdm import tqdm
 
 def feature_combination(df_list):
     result = df_list[0]
-    for df in df_list[1:]:
+    for df in tqdm(df_list[1:], total=len(df_list[1:])):
         if df is None or df.shape[0] == 0:
             continue
         assert(result.shape[0] == df.shape[0])
