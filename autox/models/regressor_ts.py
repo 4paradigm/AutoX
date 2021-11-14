240b35118c573b6810db4b05558c0034d524c294
poteman <946691288@qq.com>
2021-11-14 19:59:02 +0800

    时序模型: 时间单位(间隔)为week.

--- autox/models/regressor_ts.py
+++ autox/models/regressor_ts.py
@@ -142,6 +142,8 @@ class LgbRegressionTs(object):
 
         if ts_unit == 'D':
             one_unit = timedelta(days=1)
+        elif ts_unit == 'W':
+            one_unit = timedelta(days=7)
         intervals = int((pd.to_datetime(test[time_col].max()) - pd.to_datetime(test[time_col].min())) / one_unit + 1)
         split_time = pd.to_datetime(train[time_col].max()) - intervals * one_unit
         train_idx = train.loc[~(train[time_col] > str(split_time))].index
