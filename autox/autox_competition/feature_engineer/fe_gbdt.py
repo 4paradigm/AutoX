0d62080f3be947dbdcff739b1517e1e7d3b61f62
poteman <946691288@qq.com>
2022-02-25 16:34:09 +0800

    debug fe_gbdt.

--- autox/autox_competition/feature_engineer/fe_gbdt.py
+++ autox/autox_competition/feature_engineer/fe_gbdt.py
@@ -12,7 +12,11 @@ class FeatureGbdt:
     def fit(self, X, y, objective, category_cols = [], used_cols = [], num_of_features = 30):
 
         self.category_cols = category_cols
-        self.used_cols = used_cols
+        if len(used_cols) > 0:
+            self.used_cols = used_cols
+        else:
+            self.used_cols = list(X.columns)
+        self.used_cols = [x for x in list(X.describe().columns) if x in self.used_cols]
         self.num_of_features = num_of_features
 
         assert(objective in ['binary', 'regression'])
@@ -27,20 +31,14 @@ class FeatureGbdt:
                   'feature_fraction': 0.7,
                   'feature_fraction_seed': 66,
                   'max_bin': 100,
-                  'max_depth': 5
+                  'max_depth': 3
                   }
         self.N_round = int(num_of_features)
-        if len(self.used_cols) > 0:
-            trn_data = lgb.Dataset(X[self.used_cols], label=y, categorical_feature=category_cols)
-        else:
-            trn_data = lgb.Dataset(X, label=y, categorical_feature=category_cols)
+        trn_data = lgb.Dataset(X[self.used_cols], label=y, categorical_feature=category_cols)
         self.clf = lgb.train(params, trn_data, num_boost_round=self.N_round, valid_sets=[trn_data], verbose_eval=10)
 
     def transform(self, X):
-        if len(self.used_cols) > 0:
-            lgb_feature = pd.DataFrame(self.clf.predict(X[self.used_cols], pred_leaf=True))
-        else:
-            lgb_feature = pd.DataFrame(self.clf.predict(X, pred_leaf=True))
+        lgb_feature = pd.DataFrame(self.clf.predict(X[self.used_cols], pred_leaf=True))
 
         feas_name = ["lgb_" + str(i) for i in range(1, self.N_round + 1)]
         lgb_feature.columns = feas_name
