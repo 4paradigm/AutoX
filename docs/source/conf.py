f138e06cc0ec34fd4af0a4661114917730aedac1
poteman <946691288@qq.com>
2022-04-05 09:12:17 +0800

    sphinx version.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,6 +26,9 @@ author = 'caihengxing'
 
 # -- General configuration ---------------------------------------------------
 
+# If your documentation needs a minimal Sphinx version, state it here.
+needs_sphinx = '4.4.0'
+
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
