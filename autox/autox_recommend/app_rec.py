9d8d720405e31e7bebca670b4f050b20455f1b60
poteman <946691288@qq.com>
2022-05-18 11:53:59 +0800

    update app_rec.

--- autox/autox_recommend/app_rec.py
+++ autox/autox_recommend/app_rec.py
@@ -3,27 +3,30 @@ from flask_restful import reqparse, abort, Api, Resource
 import pickle
 import numpy as np
 
+import sys
+
+sys.path.append('../AutoX')
 from autox import AutoXRecommend
 
 app = Flask(__name__)
 api = Api(app)
-
 
 autoXRecommend = AutoXRecommend()
 
 path = 'autoXRecommend_popular_temp'
 autoXRecommend.load(path)
 
-
 # argument parsing
 parser = reqparse.RequestParser()
 parser.add_argument('uids')
+
 
 class PredictSentiment(Resource):
     def get(self):
         # use parser and find the user's query
         args = parser.parse_args()
         uids = args['uids']
+        uids = uids.split(' ')
         print(uids)
 
         res = autoXRecommend.transform(uids)
@@ -47,12 +50,12 @@ class PredictSentiment(Resource):
         # # create JSON object
         # output = {'prediction': pred_text, 'confidence': confidence}
 
-        return res
+        return res.to_json(orient="index")
+
 
 # Setup the Api resource routing here
 # Route the URL to the resource
 api.add_resource(PredictSentiment, '/')
-
 
 if __name__ == '__main__':
     app.run(debug=False)
