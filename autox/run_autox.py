06cb00ce931000233fd488de0a640c721fe44420
poteman <946691288@qq.com>
2022-12-21 10:40:02 +0800

    update: shell脚本执行autox.

--- autox/run_autox.py
+++ autox/run_autox.py
@@ -9,7 +9,7 @@ ap.add_argument('test_path', nargs=1, action="store", type=str)
 ap.add_argument('output_path', nargs=1, action="store", type=str)
 ap.add_argument('exp', nargs=1, action="store", type=str)
 ap.add_argument('id', nargs=1, action="store", type=str)
-ap.add_argument('label', nargs=1, action="store", type=str)
+ap.add_argument('target', nargs=1, action="store", type=str)
 
 pa = ap.parse_args()
 train_path = pa.train_path[0]
@@ -17,7 +17,7 @@ test_path = pa.test_path[0]
 output_path = pa.output_path[0]
 exp = pa.exp[0]
 id = pa.id[0]
-label = pa.label[0]
+target = pa.target[0]
 
 train_name = train_path.split('/')[-1]
 test_name = test_path.split('/')[-1]
@@ -25,8 +25,8 @@ test_name = test_path.split('/')[-1]
 train = pd.read_csv(train_path)
 test = pd.read_csv(test_path)
 
-autox = AutoX(target = 'target',
-              id=[id],
+autox = AutoX(id=[id],
+              target = target,
               train_name = train_name, test_name = test_name,
               path = None,
               dfs={train_name: train, test_name: test})
@@ -36,3 +36,10 @@ output_file = os.path.join(output_path, f'{exp}.csv')
 sub.to_csv(output_file, index=False)
 
 # conda run -n myenv python script.py
+# python run_autox.py \
+#        --train_path /home/caihengxing/data/santander/train.csv \
+#        --test_path /home/caihengxing/data/santander/test.csv \
+#        --output_path /home/caihengxing/temp \
+#        --exp santander_exp1 \
+#        --id ID_code \
+#        --target target
