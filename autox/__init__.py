621e50c1e75a07bb106449aa1973103eded17a21
poteman <946691288@qq.com>
2023-01-21 10:27:29 +0800

    init AutoXReload.

--- autox/__init__.py
+++ autox/__init__.py
@@ -2,3 +2,4 @@ from .autoxcompetition import AutoX
 from .autots import AutoTS
 from .autoxserver import AutoXServer
 from .autoxrecommend import AutoXRecommend
+from .autoxcompetition_reload import AutoXReload
